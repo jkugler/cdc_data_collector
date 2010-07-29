c6b468957dd694bacbe49e357d312c660284e2c3
Joshua Kugler <joshua@azariah.com>
2010-07-28 22:32:35 -0800

    AveragingSensor works ow sensor skeleton

--- cchrc/sensors/__init__.py
+++ cchrc/sensors/__init__.py
@@ -1,4 +1,6 @@
+import collections
 import threading
+import time
 
 import cchrc.common.mod
 
@@ -12,6 +14,7 @@ class SensorBase(object):
     """
     This will be a base class and will probably never be instantiated directly.
     """
+    sensor_type = 'base'
     def __init__(self, sensor_id, name, **kwargs):
         raise NotImplemented
 
@@ -26,16 +29,40 @@ class AveragingSensor(threading.Thread):
     A "sensor" that will average over the given time period and return
     the average when asked for its reading
     """
-    def __init__(self, sensor, time_period=15*60, samples=12):
+    def __init__(self, sensor, time_period=15*60, num_samples=12):
         """
         sensor is a Sensor object
         time_period is in seconds
         samples is the number of samples to take in time_period.
         """
-        self.reading = None
+        self.sensor = sensor
+        self.readings = collections.deque([], num_samples)
+        self.end_thread = False
+        self.start_time = None
+        #self.check_interval = int(time_period/num_samples)
+        self.check_interval = time_period/float(num_samples)
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.start_time = time.time()
+        while True:
+            if self.end_thread:
+                return
+            self.readings.append(self.sensor.get_reading())
+            time.sleep(self.check_interval - ((time.time() - self.start_time) % self.check_interval))
 
     def get_reading(self):
         """
-        Return current value of self.average
+        Return current average
         """
-        pass
+        if not self.readings:
+            return None
+
+        readings = list(self.readings)
+        return sum(readings)/len(readings)
+
+    def go(self):
+        self.start()
+
+    def stop(self):
+        self.end_thread = True
--- cchrc/sensors/ow.py
+++ cchrc/sensors/ow.py
@@ -3,6 +3,7 @@
 import cchrc.sensors
 
 class Sensor(cchrc.sensors.SensorBase):
+    sensor_type = 'ow'
     def __init__(self, sensor_id, name, **kwargs):
         pass
 
