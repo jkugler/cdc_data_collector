8d1a34bae4b1c760d961fa37867db272697b12a4
Joshua Kugler <joshua@azariah.com>
2010-09-14 20:31:54 -0800

    Slight test restructure. Empty integration test file.

--- run_tests.py
+++ run_tests.py
@@ -8,8 +8,14 @@ import unittest
 opd = os.path.dirname
 sys.path.insert(0, opd(os.path.abspath(__file__)))
 
-from cchrc import tests as cchrc_tests
+from cchrc import unit_tests
+from cchrc import integration_tests
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromModule(cchrc_tests)
+    if len(sys.argv) > 1 and sys.argv[1] == '--integration':
+        to_run = integration_tests
+    else:
+        to_run = unit_tests
+
+    suite = unittest.TestLoader().loadTestsFromModule(to_run)
     unittest.TextTestRunner(verbosity=2).run(suite)
