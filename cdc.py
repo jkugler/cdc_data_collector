0d36b13bbcd5cf9c040d7ac0d469dec20c9832df
Joshua Kugler <joshua@azariah.com>
2010-10-14 00:53:33 -0800

    Added a *bunch* of logging statements to cdc.py, and corrected one errant variable name

--- cdc.py
+++ cdc.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 from collections import defaultdict
-import signal
+import logging
 import optparse
 import os
+import signal
 import sys
 import time
 
@@ -21,9 +22,13 @@ def get_opts():
     usage = 'usage: %prog [options] path-to-ini'
     parser = optparse.OptionParser(usage=usage)
     a = parser.add_option
+    a('-r', '--run-in-forground', action='store_true', dest='run_in_foreground',
+      help="Stay in the foreground and log to the console. Exit via Ctrl-C")
     a('-t', '--test', action='store_true', dest='test',
       help='Run CDC in test mode. (Sampling every 60 seconds)')
-    parser.set_defaults(test=False)
+    a('-v', action='count', dest='verbose', help = 'Verbosity. '
+      "Default is 'warning'; specify once for 'info' and twice for 'debug'")
+    parser.set_defaults(run_in_foreground=False, test=False, verbose=0)
 
     opts, args = parser.parse_args()
 
@@ -32,28 +37,68 @@ def get_opts():
 
     return opts, args
 
+def configure_logging(cfg, opts):
+    """Configures the root logger and returns it"""
+    LEVELS = {'debug': logging.DEBUG,
+              'info': logging.INFO,
+              'warning': logging.WARNING,
+              'error': logging.ERROR,
+              'critical': logging.CRITICAL}
+    log_args = {}
+    log_format = cfg['Main'].get('LogFormat',
+                                 "%(asctime)s %(name)s [%(levelname)s] %(message)s")
+
+    if not opts.run_in_foreground:
+        log_dir = cfg['Main'].get('LogDir', '/var/log/cchrc_data_collector')
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        log_args['filename'] = os.path.join(log_dir, 'cdc.log')
+
+    if opts.verbosity != 0:
+        log_level = max([logging.WARNING - 10 * opts.verbosity, logging.DEBUG])
+    else:
+        cfg_log_level = cfg['Main'].get('LogLevel', 'warning').lower()
+        if cfg_log_level not in LEVELS:
+            print "Invalid log level '%s' used in config files. Defaulting to 'warning'" % cfg_log_level
+            cfg_log_level = 'warning'
+        log_level = LEVELS[cfg_log_level]
+
+    log_args['level'] = log_level
+    log_args['format'] = log_format
+
+    logging.basicConfig(**log_args)
+
+    logger = logging.getLogger('cdc')
+
+    return logger
+
 class Mother(object):
     def __init__(self, start_callables, stop_callables):
         self.start_callables = start_callables
         self.stop_callables = stop_callables
         self.ended = False
+        self.log = logging.getLogger('cdc.Mother')
 
     def start(self):
         for c in self.start_callables:
+            self.log.debug("Mother.start starting '%s'", str(c))
             c()
 
     def stop(self, sig, stack_frame):
         for c in self.stop_callables:
+            self.log.debug("Mother.start stopping '%s'", str(c))
             c()
         self.ended = True
 
 def main():
+    opts, args = get_opts()
+    cfg = cchrc.common.config.Config(args[0])
+
+    log = configure_logging(cfg, opts)
+
     DF = cchrc.common.datafile.DataFile
     sc = cchrc.common.SensorContainer()
     dfr = cchrc.common.datafile.DataFileRunner()
-    opts, args = get_opts()
-
-    cfg = cchrc.common.config.Config(args[0])
 
     for group in cfg['SensorGroups']:
         if '/' in cfg['SensorGroups'][group]['SensorType']:
@@ -61,6 +106,9 @@ def main():
         else:
             stype = cfg['SensorGroups'][group]['SensorType']
             group_params = {}
+
+        log.debug("Configuring group '%s' with SensorType '%s' and params '%s'",
+                  group, stype, str(group_params))
 
         sensors = cfg['SensorGroups'][group]['Sensors']
         for sensor in sensors:
@@ -75,9 +123,15 @@ def main():
             all_params.update(group_params)
             all_params.update(sensor_params)
             # TODO: This can throw a KeyError...catch it.
+            log.debug("Configuring sensor '%s' with id '%s' and params '%s'",
+                      name, sensor_id, str(all_params))
             sobject = cchrc.sensors.get(stype).Sensor(name, sensor_id, **all_params)
             if group + '.' + name in cfg['Names']:
-                sobject.display_name = cfg['Names'][group + '.' + name]
+                display_name = cfg['Names'][group + '.' + name]
+                log.debug("Giving sensor '%s' display name '%s'",
+                          name, display_name)
+                sobject.display_name = display_name
+            log.debug("Putting sensor '%s' in SC", name)
             sc.put(sobject, group)
 
     for data_file in cfg['Files']:
@@ -85,7 +139,9 @@ def main():
         if opts.test:
             sampling_time = 60
         else:
-            sample_time = int(fcfg['SamplingTime'])
+            sampling_time = int(fcfg['SamplingTime'])
+        log.debug("Configuring datafile '%s' with sampling time %s",
+                  data_file, sampling_time)
         dfr.put(DF(data_file, fcfg['FileName'], cfg['Main']['BaseDirectory'],
                    fcfg['DefaultGroup'], sampling_time,
                    fcfg['DefaultMode'], listify(fcfg['Sensors']), sc))
@@ -93,6 +149,7 @@ def main():
     mom = Mother([sc.start_averaging_sensors, dfr.start_data_files],
                  [sc.stop_averaging_sensors, dfr.stop_data_files])
 
+    log.debug("Starting Mother")
     mom.start()
 
     signal.signal(signal.SIGTERM, mom.stop)
@@ -101,9 +158,12 @@ def main():
     while True:
         # Putting a 'while True' loop here for "future expansion," such as
         # printing out system stats on a SIGUSR1 or some such
+        log.debug("Waiting for signals")
         signal.pause()
         if mom.ended:
+            log.debug("Mother ended. Shutting down")
             return
 
 if __name__ == '__main__':
     main()
+    logging.shutdown()
